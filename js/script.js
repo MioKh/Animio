8e2db1f477504638efe462c087bd1460536cd92c
MioKh <mioakiyamakh@gmail.com>
2024-04-16 03:01:18 +0300

    Added simple routing & active link highlighting

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log(window.location.pathname);
+const global = {
+  currentPage: window.location.pathname,
+};
+
+function highlightActiveLink(){
+ const links = document.querySelectorAll('.nav-link');
+ links.forEach(link => {
+    if(link.getAttribute('href') === global.currentPage){
+        link.classList.add('active');
+    }
+ });
+}
+
+//initlaize app
+function init() {
+  switch (global.currentPage) {
+    case "/":
+    case '/index.html':
+      console.log("home");
+      break;
+    case "/manga.html":
+      console.log("mangas");
+      break;
+    case "/anime-details.html":
+      console.log("anime-details");
+      break;
+    case "/manga-details.html":
+      console.log("manga-details");
+      break;
+    case "/search.html":
+      console.log('search');
+      break;
+  }
+  highlightActiveLink();
+}
+
+
+document.addEventListener("DOMContentLoaded", init);
