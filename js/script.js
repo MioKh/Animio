5fa194d38829e7f5bca47d2110742f926882f786
Amro Khalfa <mioakiyamakh@gmail.com>
2024-04-16 23:58:32 +0300

    Implemented API data fetching and display on website.

--- js/script.js
+++ js/script.js
@@ -2,35 +2,67 @@ const global = {
   currentPage: window.location.pathname,
 };
 
+async function displayPopularAnimes(){
+  const { data } = await fetchApiData('top/anime');
+  
+  console.log(data);
+  data.forEach((anime) =>{
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="anime-details.html?id=${anime.mal_id}">
+      <img
+        src="${anime.images.jpg.large_image_url}"
+        alt="${anime.title}"
+      />
+    </a>
+    <div class="card-body">
+      <h5 class="card-title">${anime.title}</h5>
+      <p class="card-text">
+        <small class="text-muted">Release: ${anime.aired.from.slice(0, 10)}</small>
+      </p>
+    </div>`
+    document.querySelector('#popular-animes').appendChild(div);
+  })
+}
+
+async function fetchApiData(endpoint){
+  const API_URL = `https://api.jikan.moe/v4/`;
+
+  const respone = await fetch(`${API_URL}${endpoint}`);
+  const data = await respone.json();
+  return data; 
+}
+
 function highlightActiveLink(){
  const links = document.querySelectorAll('.nav-link');
  links.forEach(link => {
-    if(link.getAttribute('href') === global.currentPage){
+    if(link.href === window.location.origin + global.currentPage){
         link.classList.add('active');
     }
  });
 }
-
 //initlaize app
 function init() {
   switch (global.currentPage) {
     case "/":
-    case '/index.html':
-      console.log("home");
+    case "/index.html":
+      displayPopularAnimes(); 
       break;
-    case "/manga.html":
+    case "manga":
       console.log("mangas");
       break;
-    case "/anime-details.html":
+    case "anime-details":
       console.log("anime-details");
       break;
-    case "/manga-details.html":
+    case "manga-details":
       console.log("manga-details");
       break;
-    case "/search.html":
+    case "search":
       console.log('search');
       break;
   }
+
   highlightActiveLink();
 }
 
