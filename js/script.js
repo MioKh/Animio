a8a4e0a7f6635274e70580b5c066e7c61f22fc94
Amro Khalfa <mioakiyamakh@gmail.com>
2024-04-19 22:43:42 +0300

    recommendation slider progress

--- js/script.js
+++ js/script.js
@@ -8,7 +8,6 @@ const path = global.currentPage;
 
 async function displayPopularAnimes() {
   const { data } = await fetchApiData("top/anime");
-
   console.log(data);
   data.forEach((anime) => {
     const div = document.createElement("div");
@@ -16,9 +15,9 @@ async function displayPopularAnimes() {
     div.innerHTML = `
       <a href="anime-details.html?id=${anime.mal_id}">
       <img 
-      src="${anime.images?.jpg?.large_image_url || 'images/no-image.jpg'}" 
+      src="${anime.images?.jpg?.large_image_url || "images/no-image.jpg"}" 
       class="card-img-top" 
-      alt="${anime.title || 'No Image Available'}" 
+      alt="${anime.title || "No Image Available"}" 
     />
     </a>
     <div class="card-body">
@@ -44,9 +43,9 @@ async function displayPopularMangas() {
     div.innerHTML = `
       <a href="manga-details.html?id=${manga.mal_id}">
       <img 
-        src="${manga.images?.jpg?.large_image_url || 'images/no-image.jpg'}" 
+        src="${manga.images?.jpg?.large_image_url || "images/no-image.jpg"}" 
         class="card-img-top" 
-        alt="${manga.title || 'No Image Available'}" 
+        alt="${manga.title || "No Image Available"}" 
       />
     </a>
     <div class="card-body">
@@ -66,19 +65,23 @@ async function displayAnimeDetails() {
   const animeId = window.location.search.split("=")[1];
   const { data } = await fetchApiData(`anime/${animeId}/full`);
   console.log(data);
-
   const div = document.createElement("div");
   div.innerHTML = `<div class="details-top">
   <div>
       <img 
-        src="${data.images?.jpg?.large_image_url || 'images/no-image.jpg'}" 
+        src="${data.images?.jpg?.large_image_url || "images/no-image.jpg"}" 
         class="card-img-top" 
-        alt="${data.title || 'No Image Available'}" 
+        alt="${data.title || "No Image Available"}" 
       />
   </div>
   <div>
       <h2>${data.title}</h2>
-      <h3>${data.title_english || data.titles.english || data.titles.synonym || data.title}</h3>
+      <h3>${
+        data.title_english ||
+        data.titles.english ||
+        data.titles.synonym ||
+        data.title
+      }</h3>
       <p>
           <i class="fas fa-star text-primary"></i>
           ${data.score}/10
@@ -89,9 +92,7 @@ async function displayAnimeDetails() {
       </p>
       <h5>Genres</h5>
       <ul class="list-group">
-          ${data.genres
-            .map((genre) => `<li>${genre.name}</li>`)
-            .join("")}
+          ${data.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
       </ul>
       <a href="${
         data.external[0].name != "Official Site"
@@ -103,19 +104,38 @@ async function displayAnimeDetails() {
 <div class="details-bottom">
   <h2>Anime Info</h2>
   <ul>
-      <li><span class="text-secondary">Episodes:</span> ${data.episodes} episode</li>
+      <li><span class="text-secondary">Episodes:</span> ${
+        data.episodes
+      } episode</li>
       <li><span class="text-secondary">Rating:</span> ${data.rating}</li>
       <li><span class="text-secondary">Duration:</span> ${data.duration}</li>
       <li><span class="text-secondary">Status:</span> ${data.status}</li>
   </ul>
   <h4>Production Companies</h4>
-  <div class="list-group">${data.producers.map((producer) => producer.name).join(" | ")}</div>
+  <div class="list-group">${data.producers
+    .map((producer) => producer.name)
+    .join(" | ")}</div>
 </div>`;
 
   document.querySelector("#Anime-details").appendChild(div);
 }
+// test function for displaying backdrop images in anime details
+/* async function displayAnimeImages(){
+   const animeId = window.location.search.split("=")[1];
+   const { data } = await fetchApiData(`anime/${animeId}/pictures`);
+   const images = data.filter(picture => picture.jpg.large_image_url);
+   const randomImage = images[Math.floor(Math.random() * images.length)];
+   const div = document.createElement("div");
+   const imageContainer = document.createElement("div");
+   imageContainer.classList.add("image-container");
+   const img = document.createElement("img");
+   img.src = randomImage.jpg.large_image_url;
+   imageContainer.appendChild(img);
+   div.appendChild(imageContainer);
+   document.querySelector("#Anime-details").appendChild(div);
+*/
 
-async function displayMangaDetails(){
+async function displayMangaDetails() {
   const mangaId = window.location.search.split("=")[1];
   const { data } = await fetchApiData(`manga/${mangaId}/full`);
   console.log(data);
@@ -124,14 +144,19 @@ async function displayMangaDetails(){
   div.innerHTML = ` <div class="details-top">
   <div>
     <img
-      src="${data.images?.jpg?.large_image_url || 'images/no-image.jpg'}"
+      src="${data.images?.jpg?.large_image_url || "images/no-image.jpg"}"
       class="card-img-top"
-      alt="${data.title || 'No Image Available'}"
+      alt="${data.title || "No Image Available"}"
     />
   </div>
   <div>
     <h2>${data.title}</h2>
-    <h3>${data.title_english || data.titles.english || data.titles.synonym || data.title}</h3>
+    <h3>${
+      data.title_english ||
+      data.titles.english ||
+      data.titles.synonym ||
+      data.title
+    }</h3>
     <p>
       <i class="fas fa-star text-primary"></i>
       ${data.score} / 10
@@ -142,23 +167,23 @@ async function displayMangaDetails(){
     </p>
     <h5>Genres</h5>
     <ul class="list-group">
-      ${data.genres
-        .map((genre) => `<li>${genre.name}</li>`)
-        .join("")}
+      ${data.genres.map((genre) => `<li>${genre.name}</li>`).join("")}
     </ul>
     <a href="${
-      data.external[0].name != "Official Site"
-        ? data.url
-        : data.external[0].url
+      data.external[0].name != "Official Site" ? data.url : data.external[0].url
     }" target="_blank" class="btn">Visit Anime Homepage</a>
   </div>
 </div>
 <div class="details-bottom">
   <h2>Manga Info</h2>
   <ul>
-    <li><span class="text-secondary">Number Of Volumes:</span> ${data.volumes ? data.volumes : 'N/A' }</li>
+    <li><span class="text-secondary">Number Of Volumes:</span> ${
+      data.volumes ? data.volumes : "N/A"
+    }</li>
     <li>
-      <span class="text-secondary">Number Of Chapters:</span> ${data.chapters ? data.chapters : 'N/A' }
+      <span class="text-secondary">Number Of Chapters:</span> ${
+        data.chapters ? data.chapters : "N/A"
+      }
     </li>
     <li><span class="text-secondary">Status:</span> ${data.status}</li>
   </ul>
@@ -169,6 +194,29 @@ async function displayMangaDetails(){
   document.querySelector("#manga-details").appendChild(div);
 }
 
+// display slider
+async function displaySlider() {
+  const { data } = await fetchApiData("recommendations/anime");
+  console.log(data);
+  data.forEach((anime) => {
+    const div = document.createElement("div");
+    div.classList.add("swiper-slide");
+
+    div.innerHTML = `
+    <a href="movie-details.html?id=${anime.mal_id}">
+      <img src="${anime.images.jpg.large_image_url}" alt="${anime.title}" />
+    </a>
+    <h4 class="swiper-rating">
+      <i class="fas fa-star text-secondary"></i> ${anime.score} / 10
+    </h4>
+    `;
+
+    // document.querySelector(".swiper-wrapper").appendChild(div);
+    // (WIP) today maybe ?
+  });
+}
+
+// gets the base api link ( then we concatenate with the endpoint )
 async function fetchApiData(endpoint) {
   const API_URL = `https://api.jikan.moe/v4/`;
   showSpinner();
@@ -178,10 +226,11 @@ async function fetchApiData(endpoint) {
   return data;
 }
 
+//show spinner
 function showSpinner() {
   document.querySelector(".spinner").classList.add("show");
 }
-
+// hide the spinner
 function hideSpinner() {
   document.querySelector(".spinner").classList.remove("show");
 }
@@ -195,23 +244,22 @@ function highlightActiveLink() {
   });
 }
 
-//initlaize app
+//a simple router function
 function init() {
   if (/^\/(Animio\/)?(index\.html)?$/.test(path)) {
     displayPopularAnimes();
+    displaySlider();
   } else if (/^\/(Animio\/)?manga\.html(\/)?$/.test(path)) {
     displayPopularMangas();
   } else if (/^\/(Animio\/)?anime-details\.html(\/)?$/.test(path)) {
     displayAnimeDetails();
+    // displayAnimeImages();
   } else if (/^\/(Animio\/)?manga-details\.html(\/)?$/.test(path)) {
     displayMangaDetails();
     console.log("manga-details");
   } else if (/^\/(Animio\/)?search\.html(\/)?$/.test(path)) {
     console.log("search");
   }
-
-  // AAA DAMN IT so i have been trying to fix this for a while and the fix is to add more cases cause github adds a whole new root file ? like if i run this localy it will work , but like the way i am using routing it changes in github cause for example index.html or just / becomes /Animio/index.html or /Animio/ , and i believe its same for all other pages
-
   highlightActiveLink();
 }
 
